d26efe27dfce714ad78ab0a6992de9f53dcad2eb
Ankesh <ankeshkumar3221@gmail.com>
2025-05-12 22:37:47 +0530

    Data change

--- src/test/java/com/medimention/utlities/DataProviders.java
+++ src/test/java/com/medimention/utlities/DataProviders.java
@@ -9,7 +9,7 @@ public class DataProviders {
 	@DataProvider(name="RegData")
 	public String[][] getData() throws IOException
 	{
-		String path= ".\\TestData\\MOCK_DATA.xlsx";
+		String path= ".\\TestData\\MOCK_DATA (1).xlsx";
 		
 		ExcelUtility xlutils= new ExcelUtility(path);
 		
