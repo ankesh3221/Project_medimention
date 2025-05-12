1b0f53c5dd48a48b9c73d6462d17cc8d72842c32
Ankesh <ankeshkumar3221@gmail.com>
2025-05-12 23:32:47 +0530

    commit

--- src/test/java/com/medimention/testcases/baseClass.java
+++ src/test/java/com/medimention/testcases/baseClass.java
@@ -1,15 +1,22 @@
 package com.medimention.testcases;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 import java.util.Properties;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -19,6 +26,7 @@ public class baseClass {
 	public WebDriver driver;
 	//String url = "https://www.medimention.com/";
 	public Properties p;
+	
 	@BeforeClass(groups = {"Sanity","Regression","DataDriven","smoke"})
 	@Parameters({"os","browser",})
 	public void setUp(String os ,String br) throws IOException, InterruptedException {
@@ -47,17 +55,19 @@ public class baseClass {
 	
 	@AfterClass
 	public void tearDown() {
+//		if (ITestResult.FAILURE == result.getStatus()) {
+//			System.out.println(result);
+//            takeScreenshot(result.getName());
+//        }
 		driver.quit();
 	}
 	
 	public String randomStr() {
-		
 		String createStr = RandomStringUtils.randomAlphabetic(6);
 		return createStr;
 	}
 	
 	public String randomNum() {
-		
 		String createNum = RandomStringUtils.randomNumeric(10);
 		return createNum;
 	}
@@ -67,5 +77,19 @@ public class baseClass {
 		return actions;
 	}
 	
-	
+//	public void takeScreenshot(String testName) {
+//        File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+//        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+//        File destFile = new File("screenshots/" + testName + "_" + timestamp + ".png");
+//
+//        destFile.getParentFile().mkdirs();  // Ensure directory exists
+//        try {
+//            Files.copy(srcFile.toPath(), destFile.toPath());
+//            System.out.println("Screenshot saved to: " + destFile.getAbsolutePath());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
+	
+
